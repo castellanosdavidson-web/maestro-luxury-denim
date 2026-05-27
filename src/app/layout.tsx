420e9f81df7daab80c729f2a1dcf39f05ac51e2a
Maestro Admin <admin@maestro.com>
2026-05-27 11:35:27 -0500

    chore: add Meta Pixel tracking code to main layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Playfair_Display, Inter, Geist } from "next/font/google";
 import { CartProvider } from "@/context/CartContext";
 import CartSidebar from "@/components/cart/CartSidebar";
@@ -70,7 +71,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es" className={cn(playfair.variable, inter.variable, "font-sans", geist.variable)}>
+      <head>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '2018668862108446');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+      </head>
       <body className="min-h-screen flex flex-col antialiased selection:bg-maestro-gold selection:text-maestro-dark">
+        <noscript>
+          <img height="1" width="1" style={{ display: "none" }} src="https://www.facebook.com/tr?id=2018668862108446&ev=PageView&noscript=1" />
+        </noscript>
         <SmoothScroll>
           <CartProvider>
             {children}
